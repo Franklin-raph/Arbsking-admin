c78b30a2730c1492c0cec024d7fdc9e08d31b480
franklin-raph <igboekwulusifranklin@gmail.com>
2023-06-27 13:57:00 +0100

    updated the admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Register from './pages/authentication/Register'
 import Login from './pages/authentication/Login'
 import Dashboard from './pages/layout/Dashboard'
+import Sidenav from './components/sideNav'
 import './App.css'
 
 function App() {
 
+  const loggedInAdmin = JSON.parse(localStorage.getItem('admin'))
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+        getAllUsers()
+  },[])
+
+  async function getAllUsers(){
+    const response = await fetch("https://sportbetpredict.onrender.com/api/admin/fetch/all-users", {
+      method:"POST",
+      headers: {
+        Authorization: `Bearer ${loggedInAdmin}`
+      }
+    })
+    console.log(response)
+    const data = await response.json()
+    if(response.ok){
+      setUsers(data.message)
+      console.log(users)
+    }
+    console.log(data)
+  }
+
   return (
     <>
       <BrowserRouter>
+      <Sidenav users={users}/>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/register' element={<Register />} />
-          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/dashboard' element={<Dashboard users={users}/>} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/sideNav/index.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Sidenav = ({users}) => {
+
+  useEffect(() => {
+  },[])
+
+  const totalActiveUsers = users.filter(user => user.status === "activated").length;
+
+  return (
+    <div>
+      <div className="d-flex justify-content-between p-3 mb-4" style={{ borderBottom:"1px solid grey" }}v>
+        <h4 className='text-light'>Arbsking Admin</h4>
+        <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+          <i className="fa-solid fa-bars"></i>
+        </button>
+      </div>
+
+      <div className='stats'>
+        <div className="totalUsers">
+          <div>
+            <h6>{users && users.length}</h6>
+            <p>Total Users</p>
+          </div>
+          <i className="fa-solid fa-users"></i>
+        </div>
+        <div className="totalActiveUsers">
+          <div>
+            <h6>{users && totalActiveUsers}</h6>
+            <p>Activated Users</p>
+          </div>
+          <i className="fa-solid fa-circle-check"></i>
+        </div>
+        <div className="totalUsers">
+          <div>
+            <h6>{users && users.length}</h6>
+            <p>Subscribed Users</p>
+          </div>
+          <i className="fa-solid fa-users"></i>
+        </div>
+        <div className="totalUsers">
+          <div>
+            <h6>{users && users.length}</h6>
+            <p>Un-Sub. Users</p>
+          </div>
+          <i className="fa-solid fa-users"></i>
+        </div>
+      </div>
+
+      <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Menu</h5>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div className="offcanvas-body">
+          <Link to="/dashboard">All Users</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidenav
